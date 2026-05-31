88f1b7b9676ad5811673eae203a5433b89e31452
Meseret1G <162950936+Meseret1G@users.noreply.github.com>
2026-05-31 20:36:00 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
+            value: "unsafe-none",
           },
         ],
       },
