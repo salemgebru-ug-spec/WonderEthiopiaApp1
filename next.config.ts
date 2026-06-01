bf0cb0b31706512edf5964fa6bfbdf23278e95d1
Meseret1G <162950936+Meseret1G@users.noreply.github.com>
2026-06-01 17:28:07 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["mongoose"],
-  experimental: {
-    serverComponentsExternalPackages: ["@xenova/transformers"],
-  },
-  webpack: (config) => {
-    config.externals = [...(config.externals || []), "@xenova/transformers"];
-    return config;
-  },
+ 
 
   async headers() {
     return [
